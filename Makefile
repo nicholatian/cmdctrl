ca723b16274da2a47e829d38b985d6b7036c6777
Alexander Nicholi <me@nicholatian.com>
2026-04-20 17:30:51 +0700

    move site installation to script

--- Makefile
+++ Makefile
@@ -7,21 +7,10 @@ SCRIPTS := \
 	src/tls-recv.sh \
 	src/www-send.sh \
 	src/www-recv.sh
-SITES   := \
-	src/websites/mt.xion.irc.conf
 
 .PHONY:
 
 all: install
 
 install:
-	install -m755 $(SCRIPTS) $(PREFIX)/bin
-	test -f /etc/nginx/nginx.conf && \
-	mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-	cp -a src/nginx.conf /etc/nginx/nginx.conf
-	cp -a src/sslredir.conf /etc/nginx/ssl-redirect.conf
-	cp -a src/sitebits.conf /etc/nginx/sitebits.conf
-	cp -a src/lets-ssl.conf /etc/nginx/letsencrypt-ssl.conf
-	cp -a src/dhparams.pem /etc/nginx/letsencrypt-dhparams.pem
-	mkdir -p /etc/nginx/available
-	mkdir -p /etc/nginx/enabled
+	install -m755 -o root -g root $(SCRIPTS) $(PREFIX)/bin
--- util/install-www.sh
+++ util/install-www.sh
@@ -1,1 +1,43 @@
 #!/bin/sh
+
+test=/bin/test;
+command -v gtest 2>&1 >/dev/null && test=gtest;
+cp=/bin/cp;
+command -v gcp 2>&1 >/dev/null && cp=gcp;
+mv=/bin/mv;
+command -v gmv 2>&1 >/dev/null && mv=gmv;
+ln=/bin/ln;
+command -v gln 2>&1 >/dev/null && ln=gln;
+mkdir=/bin/mkdir;
+command -v gmkdir 2>&1 >/dev/null && mkdir=gmkdir;
+install=/usr/bin/install;
+command -v ginstall 2>&1 >/dev/null && install=ginstall;
+find=/usr/bin/find;
+command -v gfind 2>&1 >/dev/null && find=gfind;
+
+zero="$0";
+zero="$(cd $(dirname "$zero")/.. && pwd)";
+
+${test} -f /etc/nginx/nginx.conf && \
+	${mv} /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak;
+${cp} -a src/nginx.conf /etc/nginx/nginx.conf;
+${cp} -a src/sslredir.conf /etc/nginx/ssl-redirect.conf;
+${cp} -a src/sitebits.conf /etc/nginx/sitebits.conf;
+${cp} -a src/lets-ssl.conf /etc/nginx/letsencrypt-ssl.conf;
+${cp} -a src/dhparams.pem /etc/nginx/letsencrypt-dhparams.pem;
+${mkdir} -p /etc/nginx/available;
+${mkdir} -p /etc/nginx/enabled;
+
+CDPATH= cd "$zero/src/websites";
+sites="$(${find} -type f -name '*.conf')";
+
+CDPATH= cd /etc/nginx/available;
+for site in $sites; do
+	${cp} "$zero/src/websites/$site" ./;
+done
+CDPATH= cd ../enabled;
+for site in $sites; do
+	${ln} -sf "../available/$site";
+done
+
+CDPATH= cd "$zero";
